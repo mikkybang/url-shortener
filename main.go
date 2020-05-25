37331f125095e29108f38f7614d678b62a28e280
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-25 17:51:45 +0100

    Add initStore function to main.go

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 
 import {
+	"log",
 	"github.com/gofiber/giber",
-	"github.com/boltdb/bolt"
+	"github.com/boltdb/bolt",
+	"fmt"
 }
 
 
-func main() {
 
+func initStore(){
+	db, err := bolt.Open("store.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection to store opened")
+
+}
+
+func main() {
+	app := fiber.New()
+
+	initStore()
+	
+	defer db.Close()
 
 }
