8592ac2a95d42db20ddbf8e72125c9b62afd313a
Michael Efemena <mikkybang@localhost.localdomain>
2020-05-29 11:31:59 +0100

    use Link url as key

--- link/link.go
+++ link/link.go
@@ -2,7 +2,6 @@ package link
 
 import (
 	"encoding/json"
-	"log"
 	"time"
 
 	"github.com/boltdb/bolt"
@@ -12,18 +11,19 @@ import (
 )
 
 type Link struct {
-	Url       string    `json: "Url"`
+	Url       string    `json: "url"`
 	hash      string    `json: "hash"`
 	createdAt time.Time `json:"created_at" bson:"created_at"`
 }
 
-func createUrl(c *fiber.Ctx) {
+func CreateUrl(c *fiber.Ctx) {
 	store := store.Db
-	link := &Link
+	link := new(Link)
 	if err := c.BodyParser(link); err != nil {
 		c.Status(503).Send(err)
 		return
 	}
+	s := shortid.New()
 	link.hash = s.Generate()
 	link.createdAt = time.Now()
 
@@ -34,9 +34,9 @@ func createUrl(c *fiber.Ctx) {
 		return
 	}
 
-	err := store.Update(func(tx *bolt.Tx) error {
+	error := store.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte("Link"))
-		err := b.Put(newLink)
+		err := b.Put([]byte(link.Url), newLink)
 		return err
 	})
 
@@ -44,7 +44,7 @@ func createUrl(c *fiber.Ctx) {
 		c.Status(503).Send(err)
 		return
 	}
-
+	c.Status(200).JSON(newLink)
 }
 
 func RedirectUrl(c *fiber.Ctx) {
--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 
 	"github.com/boltdb/bolt"
 	"github.com/gofiber/fiber"
+	"github.com/mikkybang/url-shortener/link"
 	"github.com/mikkybang/url-shortener/store"
 )
 
 func setupRoutes(app *fiber.App) {
 	api := app.Group("/api")
+
+	api.Post("/create", link.CreateUrl)
 
 	api.Get("/test", func(c *fiber.Ctx) {
 		c.JSON("test")
@@ -24,11 +27,11 @@ func setupStorage() {
 	db, err := bolt.Open("store.db", 0600, nil)
 
 	store.Db = db
-	
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	fmt.Println("Connection to store opened")
 }
 
